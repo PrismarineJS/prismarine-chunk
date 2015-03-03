47a8659c82e1e45f7512a77a350b57275d5bc7ca
Will Franzen <wtfranzen@gmail.com>
2015-03-03 14:18:36 -0600

    Implement chunk code

--- src/chunk.js
+++ src/chunk.js
@@ -1,7 +1,38 @@
+const CHUNK_SIZE   = 16;
+const CHUNK_HEIGHT = 256;
+
+const BUFFER_SIZE = CHUNK_SIZE * CHUNK_SIZE * CHUNK_HEIGHT * 3;
+
+var getCursor = function(x, y, z) {
+    return (x) + (z * CHUNK_SIZE) + (y * CHUNK_HEIGHT);
+};
+
 class Chunk {
 
     constructor() {
+        this.data = new Buffer(BUFFER_SIZE);
+    }
 
+    getBlock(x, y, z) {
+        var cursor = getCursor(x, y, z);
+        return {
+            id:   this.data.readInt16BE(cursor),
+            data: this.data.readInt8(cursor + 2)
+        };
+    }
+
+    setBlock(x, y, z, block) {
+        var cursor = getCursor(x, y, z);
+        this.data.writeInt16BE(block.id, cursor);
+        this.data.writeInt8(block.data, cursor + 2);
+    }
+
+    save() {
+        return this.data;
+    }
+
+    load(data) {
+        this.data = data;
     }
 
 }
