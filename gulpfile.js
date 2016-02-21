70b16a9c8aff31e0d261247684e542c1bd30d005
Romain Beaumont <romain.rom1@gmail.com>
2016-02-21 20:45:31 +0100

    update to babel6

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,31 @@
 var gulp = require('gulp');
 
+var plumber = require('gulp-plumber');
 var babel = require('gulp-babel');
+var options = {
+  presets: ['es2015']
+};
 
-gulp.task('transpile', function() {
-    gulp.src('src/*.js')
-        .pipe(babel())
-        .pipe(gulp.dest('dist/'));
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('compile', function() {
+  return gulp
+    .src('src/**/*.js')
+    .pipe(plumber({
+      errorHandler: function(err) {
+        console.error(err.stack);
+        this.emit('end');
+      }
+    }))
+    .pipe(sourcemaps.init())
+    .pipe(babel(options))
+    .pipe(plumber.stop())
+    .pipe(sourcemaps.write('maps/'))
+    .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('watch', function() {
-    gulp.run('transpile');
-    gulp.watch('src/*.js', ['transpile']);
+  return gulp.watch('src/**/*.js', ['compile']);
 });
 
-gulp.task('default', ['transpile']);
+gulp.task('default', ['compile']);
