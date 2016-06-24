f650f671bd308b24fa2e2dc47e2b353ae1975b08
Romain Beaumont <romain.rom1@gmail.com>
2016-06-24 21:24:09 +0200

    use a single big buffer instead of multiple buffer in pe implementation

--- src/pe/0.14/chunk.js
+++ src/pe/0.14/chunk.js
@@ -40,40 +40,31 @@ var getBlockCursor = function (pos) {
 };
 
 var getBlockDataCursor = function(pos) {
-  return getArrayPosition(pos) * 0.5;
+  return BLOCK_DATA_SIZE+getArrayPosition(pos) * 0.5;
 };
 
 var getBlockLightCursor = function(pos) {
-  return getArrayPosition(pos) * 0.5;
+  return BLOCK_DATA_SIZE+REGULAR_DATA_SIZE+getArrayPosition(pos) * 0.5;
 };
 
 var getSkyLightCursor = function(pos) {
-  return getArrayPosition(pos) * 0.5;
-};
-
-var getBiomeCursor = function (pos) {
-  return ((pos.z * w) + pos.x)*4;
+  return BLOCK_DATA_SIZE+REGULAR_DATA_SIZE+SKYLIGHT_DATA_SIZE+getArrayPosition(pos) * 0.5;
 };
 
 var getHeightMapCursor = function (pos) {
-  return (pos.z * w) + pos.x;
+  return BLOCK_DATA_SIZE+REGULAR_DATA_SIZE+SKYLIGHT_DATA_SIZE+BLOCKLIGHT_DATA_SIZE+(pos.z * w) + pos.x;
 };
+
+var getBiomeCursor = function (pos) {
+  return BLOCK_DATA_SIZE+REGULAR_DATA_SIZE+SKYLIGHT_DATA_SIZE+BLOCKLIGHT_DATA_SIZE+ADDITIONAL_DATA_SIZE_DIRTY+((pos.z * w) + pos.x)*4;
+};
+
 
 class Chunk {
   constructor() {
-    this.blocks = new Buffer(BLOCK_DATA_SIZE);
-    this.data = new Buffer(REGULAR_DATA_SIZE);
-    this.skyLight = new Buffer(SKYLIGHT_DATA_SIZE);
-    this.blockLight = new Buffer(BLOCKLIGHT_DATA_SIZE);
-    this.heightMap = new Buffer(ADDITIONAL_DATA_SIZE_DIRTY);
-    this.biomeColors = new Buffer(ADDITIONAL_DATA_SIZE_COLOR);
+    this.buffer = new Buffer(BUFFER_SIZE);
 
-    this.blocks.fill(0);
-    this.data.fill(0);
-    this.skyLight.fill(0);
-    this.blockLight.fill(0);
-    this.heightMap.fill(0);
-    this.biomeColors.fill(0);
+    this.buffer.fill(0);
   }
 
   getBlock(pos) {
@@ -97,39 +88,39 @@ class Chunk {
   }
 
   getBlockType(pos) {
-    return this.blocks.readUInt8(getBlockCursor(pos));
+    return this.buffer.readUInt8(getBlockCursor(pos));
   }
 
   setBlockType(pos, id) {
-    this.blocks.writeUInt8(id,getBlockCursor(pos));
+    this.buffer.writeUInt8(id,getBlockCursor(pos));
   }
 
   getBlockData(pos) {
-    return readUInt4LE(this.data, getBlockDataCursor(pos));
+    return readUInt4LE(this.buffer, getBlockDataCursor(pos));
   }
 
   setBlockData(pos, data) {
-    writeUInt4LE(this.data, data, getBlockDataCursor(pos));
+    writeUInt4LE(this.buffer, data, getBlockDataCursor(pos));
   }
 
   getBlockLight(pos) {
-    return readUInt4LE(this.blockLight, getBlockLightCursor(pos));
+    return readUInt4LE(this.buffer, getBlockLightCursor(pos));
   }
 
   setBlockLight(pos, light) {
-    writeUInt4LE(this.blockLight, light, getBlockLightCursor(pos));
+    writeUInt4LE(this.buffer, light, getBlockLightCursor(pos));
   }
 
   getSkyLight(pos) {
-    return readUInt4LE(this.skyLight, getSkyLightCursor(pos));
+    return readUInt4LE(this.buffer, getSkyLightCursor(pos));
   }
 
   setSkyLight(pos, light) {
-    writeUInt4LE(this.skyLight, light, getSkyLightCursor(pos));
+    writeUInt4LE(this.buffer, light, getSkyLightCursor(pos));
   }
 
   getBiomeColor(pos) {
-    var color = this.biomeColors.readInt32BE(getBiomeCursor(pos)) & 0xFFFFFF;
+    var color = this.buffer.readInt32BE(getBiomeCursor(pos)) & 0xFFFFFF;
 
     return {
       r: (color >> 16),
@@ -139,57 +130,31 @@ class Chunk {
   }
 
   setBiomeColor(pos, r, g, b) {
-    this.biomeColors.writeInt32BE((this.biomeColors.readInt32BE(getBiomeCursor(pos)) & 0xFF000000) | ((r & 0xFF) << 16) | ((g & 0xFF) << 8) | (b & 0XFF), ((pos.z << 4) + pos.x) * 4);
+    this.buffer.writeInt32BE((this.buffer.readInt32BE(getBiomeCursor(pos)) & 0xFF000000)
+      | ((r & 0xFF) << 16) | ((g & 0xFF) << 8) | (b & 0XFF), getBiomeCursor(pos));
   }
 
   getBiome(pos) {
-    return (this.biomeColors.readInt32BE(getBiomeCursor(pos)) & 0xFF000000) >> 24;
+    return (this.buffer.readInt32BE(getBiomeCursor(pos)) & 0xFF000000) >> 24;
   }
 
   setBiome(pos, id) {
-    this.biomeColors.writeInt32BE((this.biomeColors.readInt32BE(getBiomeCursor(pos)) & 0xFFFFFF) | (id << 24), ((pos.z << 4) + pos.x) * 4);
+    this.buffer.writeInt32BE((this.buffer.readInt32BE(getBiomeCursor(pos)) & 0xFFFFFF) | (id << 24), ((pos.z << 4) + pos.x) * 4);
   }
 
   getHeight(pos) {
-    return this.heightMap.readUInt8(getHeightMapCursor(pos,value));
+    return this.buffer.readUInt8(getHeightMapCursor(pos,value));
   }
 
   setHeight(pos, value) {
-    this.heightMap.writeUInt8(value,getHeightMapCursor(pos));
+    this.buffer.writeUInt8(value,getHeightMapCursor(pos));
   }
 
   load(data) {
-    var offset = 0;
-
-    this.blocks = data.slice(0, BLOCK_DATA_SIZE);
-    offset += BLOCK_DATA_SIZE;
-
-    this.data = data.slice(offset, REGULAR_DATA_SIZE + offset);
-    offset += REGULAR_DATA_SIZE;
-
-    this.skyLight = data.slice(offset, SKYLIGHT_DATA_SIZE + offset);
-    offset += SKYLIGHT_DATA_SIZE;
-
-    this.blockLight = data.slice(offset, BLOCKLIGHT_DATA_SIZE + offset);
-    offset += BLOCKLIGHT_DATA_SIZE;
-
-    this.heightMap = data.slice(offset, ADDITIONAL_DATA_SIZE_DIRTY + offset);
-    offset += ADDITIONAL_DATA_SIZE_DIRTY;
-
-    this.biomeColors = data.slice(offset, ADDITIONAL_DATA_SIZE_COLOR + offset);
-    offset += ADDITIONAL_DATA_SIZE_COLOR;
-
-    return offset;
+    this.buffer=data;
   }
 
   dump() {
-    return Buffer.concat([
-      this.blocks,
-      this.data,
-      this.skyLight,
-      this.blockLight,
-      this.heightMap,
-      this.biomeColors
-    ], BUFFER_SIZE);
+    return this.buffer;
   }
 }
