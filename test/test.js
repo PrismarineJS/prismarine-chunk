3acc1356eee7be45b73479ec3cd0666e2b121ee3
Romain Beaumont <romain.rom1@gmail.com>
2018-09-29 19:00:24 +0200

    standard 12 fixes

--- test/test.js
+++ test/test.js
@@ -34,36 +34,36 @@ versions.forEach(function (version) {
     it('should default to having all blocks be air', function () {
       const chunk = new Chunk()
 
-      assert.equal(0, chunk.getBlock(new Vec3(0, 0, 0)).type)
-      assert.equal(0, chunk.getBlock(new Vec3(15, Chunk.h - 1, 15)).type)
+      assert.strictEqual(0, chunk.getBlock(new Vec3(0, 0, 0)).type)
+      assert.strictEqual(0, chunk.getBlock(new Vec3(15, Chunk.h - 1, 15)).type)
     })
     it('should set a block at the given position', function () {
       const chunk = new Chunk()
 
       chunk.setBlock(new Vec3(0, 0, 0), new Block(5, 0, 2)) // Birch planks, if you're wondering
-      assert.equal(5, chunk.getBlock(new Vec3(0, 0, 0)).type)
-      assert.equal(2, chunk.getBlock(new Vec3(0, 0, 0)).metadata)
+      assert.strictEqual(5, chunk.getBlock(new Vec3(0, 0, 0)).type)
+      assert.strictEqual(2, chunk.getBlock(new Vec3(0, 0, 0)).metadata)
 
       chunk.setBlock(new Vec3(0, 37, 0), new Block(42, 0, 0)) // Iron block
-      assert.equal(42, chunk.getBlock(new Vec3(0, 37, 0)).type)
-      assert.equal(0, chunk.getBlock(new Vec3(0, 37, 0)).metadata)
+      assert.strictEqual(42, chunk.getBlock(new Vec3(0, 37, 0)).type)
+      assert.strictEqual(0, chunk.getBlock(new Vec3(0, 37, 0)).metadata)
 
       chunk.setBlock(new Vec3(1, 0, 0), new Block(35, 0, 1)) // Orange wool
-      assert.equal(35, chunk.getBlock(new Vec3(1, 0, 0)).type)
-      assert.equal(1, chunk.getBlock(new Vec3(1, 0, 0)).metadata)
+      assert.strictEqual(35, chunk.getBlock(new Vec3(1, 0, 0)).type)
+      assert.strictEqual(1, chunk.getBlock(new Vec3(1, 0, 0)).metadata)
     })
     it('should overwrite blocks in place', function () {
       const chunk = new Chunk()
 
       chunk.setBlock(new Vec3(0, 1, 0), new Block(42, 0, 0)) // Iron block
       chunk.setBlock(new Vec3(0, 1, 0), new Block(41, 0, 0)) // Gold block
-      assert.equal(41, chunk.getBlock(new Vec3(0, 1, 0)).type)
-      assert.equal(0, chunk.getBlock(new Vec3(0, 1, 0)).metadata)
+      assert.strictEqual(41, chunk.getBlock(new Vec3(0, 1, 0)).type)
+      assert.strictEqual(0, chunk.getBlock(new Vec3(0, 1, 0)).metadata)
 
       chunk.setBlock(new Vec3(5, 5, 5), new Block(35, 0, 1)) // Orange wool
       chunk.setBlock(new Vec3(5, 5, 5), new Block(35, 0, 14)) // Red wool
-      assert.equal(35, chunk.getBlock(new Vec3(5, 5, 5)).type)
-      assert.equal(14, chunk.getBlock(new Vec3(5, 5, 5)).metadata)
+      assert.strictEqual(35, chunk.getBlock(new Vec3(5, 5, 5)).type)
+      assert.strictEqual(14, chunk.getBlock(new Vec3(5, 5, 5)).metadata)
     })
     if (version !== 'pe_1.0' && version !== '1.9') {
       it('should fail safely when load is given bad input', function () {
@@ -87,19 +87,19 @@ versions.forEach(function (version) {
         const chunk = new Chunk()
 
         chunk.setBlock(new Vec3(0, 37, 0), new Block(42, 0, 0)) // Iron block
-        assert.equal(0, chunk.getBlock(new Vec3(0, 37, 0)).metadata)
-        assert.equal(42, chunk.getBlock(new Vec3(0, 37, 0)).type)
+        assert.strictEqual(0, chunk.getBlock(new Vec3(0, 37, 0)).metadata)
+        assert.strictEqual(42, chunk.getBlock(new Vec3(0, 37, 0)).type)
         const buf = chunk.dump()
         const chunk2 = new Chunk()
 
         chunk2.load(buf, 0xFFFF)
-        assert.equal(42, chunk2.getBlock(new Vec3(0, 37, 0)).type)
-        assert.equal(0, chunk2.getBlock(new Vec3(0, 37, 0)).metadata)
+        assert.strictEqual(42, chunk2.getBlock(new Vec3(0, 37, 0)).type)
+        assert.strictEqual(0, chunk2.getBlock(new Vec3(0, 37, 0)).metadata)
 
         const buf2 = chunk2.dump()
 
         if (!buf.equals(buf2)) {
-          assert.equal(buf, buf2)
+          assert.strictEqual(buf, buf2)
         }
 
         assert(buf.equals(buf2))
