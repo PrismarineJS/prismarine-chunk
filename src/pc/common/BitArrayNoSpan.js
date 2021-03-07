1537ba1117c18c10430db3e04f9606897ec99919
Alexander Ivanov <47699394+Saiv46@users.noreply.github.com>
2021-03-07 21:59:49 +0800

    Optimize BitArrayNoSpan.js

--- src/pc/common/BitArrayNoSpan.js
+++ src/pc/common/BitArrayNoSpan.js
@@ -1,48 +1,84 @@
-// const assert = require('assert')
-const neededBits = require('./neededBits')
-
 class BitArray {
-  constructor (options) {
-    if (options === null) {
-      return
-    }
+  constructor ({ bitsPerValue, capacity, data } = {}) {
     // assert(options.bitsPerValue > 0, 'bits per value must at least 1')
     // assert(options.bitsPerValue <= 32, 'bits per value exceeds 32')
-
-    const valuesPerLong = Math.floor(64 / options.bitsPerValue)
-
-    const length = Math.ceil(options.capacity / valuesPerLong)
-    if (!options.data) {
-      options.data = Array(length * 2).fill(0)
-    }
-    const valueMask = (1 << options.bitsPerValue) - 1
-
-    this.data = options.data
-    this.capacity = options.capacity
-    this.bitsPerValue = options.bitsPerValue
-    this.valuesPerLong = valuesPerLong
-    this.valueMask = valueMask
+    this.capacity = capacity | 0
+    this.bitsPerValue = bitsPerValue | 0
+    this.valueMask = (1 << this.bitsPerValue) - 1
+    this.valuesPerLong = (64 / this.bitsPerValue) | 0
+    this.data = data
+      ? (data.buffer ? new Uint32Array(data.buffer) : Uint32Array.from(data))
+      : new Uint32Array(Math.ceil(this.capacity / this.valuesPerLong) * 2)
   }
 
-  toJson () {
-    return JSON.stringify({
-      data: this.data,
-      capacity: this.capacity,
+  length () { return this.data.length >> 1 }
+  getBitsPerValue () { return this.bitsPerValue }
+
+  toJSON () {
+    return {
       bitsPerValue: this.bitsPerValue,
-      valuesPerLong: this.valuesPerLong,
-      valueMask: this.valueMask
-    })
+      capacity: this.capacity,
+      data: this.data
+    }
   }
 
-  static fromJson (j) {
-    const parsed = JSON.parse(j)
-    const bitarray = new BitArray(null)
-    bitarray.data = parsed.data
-    bitarray.capacity = parsed.capacity
-    bitarray.bitsPerValue = parsed.bitsPerValue
-    bitarray.valuesPerLong = parsed.valuesPerLong
-    bitarray.valueMask = parsed.valueMask
-    return bitarray
+  toJson () { return JSON.stringify(this.toJSON()) }
+  static fromJson (str) { return new BitArray(JSON.parse(str)) }
+
+  get (index) {
+    // assert(index >= 0 && index < this.capacity, 'index is out of bounds')
+    const startLongIndex = (index / this.valuesPerLong) << 1
+    const indexInLong = (index - startLongIndex / 2 * this.valuesPerLong) * this.bitsPerValue
+    if (indexInLong > 31) {
+      return (this.data[startLongIndex + 1] >>> (indexInLong - 32)) & this.valueMask
+    }
+    let result = this.data[startLongIndex] >>> indexInLong
+    if (indexInLong + this.bitsPerValue > 32) {
+      // Value stretches across multiple longs
+      result |= this.data[startLongIndex + 1] << (32 - indexInLong)
+    }
+    return result & this.valueMask
+  }
+
+  set (index, value) {
+    // assert(index >= 0 && index < this.capacity, 'index is out of bounds')
+    // assert(value <= this.valueMask, 'value does not fit into bits per value')
+    const startLongIndex = (index / this.valuesPerLong) << 1
+    const indexInLong = (index - startLongIndex / 2 * this.valuesPerLong) * this.bitsPerValue
+    if (indexInLong > 31) {
+      const indexInStartLong = indexInLong - 32
+      this.data[startLongIndex + 1] =
+        ((this.data[startLongIndex + 1] & ~(this.valueMask << indexInStartLong)) |
+        ((value & this.valueMask) << indexInStartLong)) >>> 0
+      return
+    }
+    // Clear bits of this value first
+    this.data[startLongIndex] =
+      ((this.data[startLongIndex] & ~(this.valueMask << indexInLong)) |
+      ((value & this.valueMask) << indexInLong)) >>> 0
+    const endBitOffset = indexInLong + this.bitsPerValue
+    if (endBitOffset > 32) {
+      // Value stretches across multiple longs
+      this.data[startLongIndex + 1] =
+        ((this.data[startLongIndex + 1] &
+          ~((1 << (endBitOffset - 32)) - 1)) |
+        (value >> (32 - indexInLong))) >>> 0
+    }
+  }
+
+  resizeTo (bitsPerValue) {
+    // assert(bitsPerValue > 0, 'bits per value must at least 1')
+    // assert(bitsPerValue <= 32, 'bits per value exceeds 32')
+    bitsPerValue |= 0
+    const newArr = new BitArray({ bitsPerValue, capacity: this.capacity })
+    for (let i = 0; i < this.capacity; i++) {
+      const value = this.get(i)
+      if ((32 - Math.clz32(value)) > bitsPerValue) {
+        throw new Error("existing value in BitArray can't fit in new bits per value")
+      }
+      newArr.set(i, value)
+    }
+    return newArr
   }
 
   toArray () {
@@ -64,82 +100,6 @@ class BitArray {
     return bitarray
   }
 
-  get (index) {
-    // assert(index >= 0 && index < this.capacity, 'index is out of bounds')
-
-    const startLongIndex = Math.floor(index / this.valuesPerLong)
-    const indexInLong = (index - startLongIndex * this.valuesPerLong) * this.bitsPerValue
-    if (indexInLong >= 32) {
-      const indexInStartLong = indexInLong - 32
-      const startLong = this.data[startLongIndex * 2 + 1]
-      return (startLong >>> indexInStartLong) & this.valueMask
-    }
-    const startLong = this.data[startLongIndex * 2]
-    const indexInStartLong = indexInLong
-    let result = startLong >>> indexInStartLong
-    const endBitOffset = indexInStartLong + this.bitsPerValue
-    if (endBitOffset > 32) {
-      // Value stretches across multiple longs
-      const endLong = this.data[startLongIndex * 2 + 1]
-      result |= endLong << (32 - indexInStartLong)
-    }
-    return result & this.valueMask
-  }
-
-  set (index, value) {
-    // assert(index >= 0 && index < this.capacity, 'index is out of bounds')
-    // assert(value <= this.valueMask, 'value does not fit into bits per value')
-
-    const startLongIndex = Math.floor(index / this.valuesPerLong)
-    const indexInLong = (index - startLongIndex * this.valuesPerLong) * this.bitsPerValue
-    if (indexInLong >= 32) {
-      const indexInStartLong = indexInLong - 32
-      this.data[startLongIndex * 2 + 1] =
-      ((this.data[startLongIndex * 2 + 1] & ~(this.valueMask << indexInStartLong)) |
-      ((value & this.valueMask) << indexInStartLong)) >>> 0
-      return
-    }
-    const indexInStartLong = indexInLong
-
-    // Clear bits of this value first
-    this.data[startLongIndex * 2] =
-      ((this.data[startLongIndex * 2] & ~(this.valueMask << indexInStartLong)) |
-      ((value & this.valueMask) << indexInStartLong)) >>> 0
-    const endBitOffset = indexInStartLong + this.bitsPerValue
-    if (endBitOffset > 32) {
-      // Value stretches across multiple longs
-      this.data[startLongIndex * 2 + 1] =
-        ((this.data[startLongIndex * 2 + 1] &
-          ~((1 << (endBitOffset - 32)) - 1)) |
-        (value >> (32 - indexInStartLong))) >>> 0
-    }
-  }
-
-  resizeTo (newBitsPerValue) {
-    // assert(newBitsPerValue > 0, 'bits per value must at least 1')
-    // assert(newBitsPerValue <= 32, 'bits per value exceeds 32')
-
-    const newArr = new BitArray({
-      bitsPerValue: newBitsPerValue,
-      capacity: this.capacity
-    })
-    for (let i = 0; i < this.capacity; ++i) {
-      const value = this.get(i)
-      if (neededBits(value) > newArr.getBitsPerValue()) {
-        throw new Error(
-          "existing value in BitArray can't fit in new bits per value"
-        )
-      }
-      newArr.set(i, value)
-    }
-
-    return newArr
-  }
-
-  length () {
-    return this.data.length / 2
-  }
-
   readBuffer (smartBuffer) {
     for (let i = 0; i < this.data.length; i += 2) {
       this.data[i + 1] = smartBuffer.readUInt32BE()
@@ -154,10 +114,6 @@ class BitArray {
       smartBuffer.writeUInt32BE(this.data[i])
     }
     return this
-  }
-
-  getBitsPerValue () {
-    return this.bitsPerValue
   }
 }
 
