006a6d7a25976503908d2f03e9f9a913e61a5b9f
Vito Gamberini <vito@gamberini.email>
2021-08-08 19:37:09 -0400

    Add type info and bounds checks (#135)

--- types/chunk.d.ts
+++ types/chunk.d.ts
@@ -36,6 +36,7 @@ declare class Chunk {
     dumpBiomes(): Array<number>;
     dumpLight(): Buffer;
     loadLight(data: Buffer, skyLightMask: number, blockLightMask: number, emptySkyLightMask?: number, emptyBlockLightMask?: number): void;
+    loadLightParse(skyLight: Buffer[], blockLight: Buffer[], skyLightMask: number[][], blockLightMask: number[][], emptySkyLightMask: number[][], emptyBlockLightMask: number[][]): void;
     loadBiomes(newBiomesArray: Array<number>): void;
     dump(bitMap?: number, skyLightSent?: boolean): Buffer;
     load(data: Buffer, bitMap?: number, skyLightSent?: boolean, fullChunk?: boolean): void;
